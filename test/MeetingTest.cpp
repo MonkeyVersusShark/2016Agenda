c8d410e8cb2244afb768d906b5fa9c2aae754074
MakWaizeon <Makwz3@hotmail.com>
2016-08-19 00:19:48 +0800

    fix google style

--- test/MeetingTest.cpp
+++ test/MeetingTest.cpp
@@ -1,27 +1,27 @@
-#include "../include/Meeting.hpp"
+#include "Meeting.hpp"
 #include <gtest/gtest.h>
 #include <string>
 #include <vector>
 
 TEST(MeetingTest, SetAndGet) {
-    Meeting meeting;
-    meeting.setSponsor("Clown");
-    EXPECT_STREQ("Clown", meeting.getSponsor().c_str());
-    std::vector<std::string> vec;
-    for (int i = 0; i < 100; i++) {
-        char ch = i + '0';
-        std::string temp = "participator";
-        temp += ch;
-        vec.push_back(temp);
-    }
-    meeting.setParticipator(vec);
-    EXPECT_EQ(vec, meeting.getParticipator());
-    Date t_start = Date::stringToDate("2016-8-19/00:00");
-    meeting.setStartDate(t_start);
-    EXPECT_EQ(t_start, meeting.getStartDate());
-    Date t_end = Date::stringToDate("2016-8-23/23:59");
-    meeting.setEndDate(t_end);
-    EXPECT_EQ(t_end, meeting.getEndDate());
-    meeting.setTitle("Boring");
-    EXPECT_STREQ("Boring", meeting.getTitle().c_str());
+  Meeting meeting;
+  meeting.setSponsor("Clown");
+  EXPECT_STREQ("Clown", meeting.getSponsor().c_str());
+  std::vector<std::string> vec;
+  for (int i = 0; i < 100; i++) {
+    char ch = i + '0';
+    std::string temp = "participator";
+    temp += ch;
+    vec.push_back(temp);
+  }
+  meeting.setParticipator(vec);
+  EXPECT_EQ(vec, meeting.getParticipator());
+  Date t_start = Date::stringToDate("2016-8-19/00:00");
+  meeting.setStartDate(t_start);
+  EXPECT_EQ(t_start, meeting.getStartDate());
+  Date t_end = Date::stringToDate("2016-8-23/23:59");
+  meeting.setEndDate(t_end);
+  EXPECT_EQ(t_end, meeting.getEndDate());
+  meeting.setTitle("Boring");
+  EXPECT_STREQ("Boring", meeting.getTitle().c_str());
 }
--- test/UserTest.cpp
+++ test/UserTest.cpp
@@ -1,15 +1,15 @@
-#include "../include/User.hpp"
+#include "User.hpp"
 #include <gtest/gtest.h>
 #include <string>
 
 TEST(UserTest, SetAndGet) {
-    User user;
-    user.setName("Clown");
-    EXPECT_STREQ("Clown", user.getName().c_str());
-    user.setPassword("55555555");
-    EXPECT_STREQ("55555555", user.getPassword().c_str());
-    user.setEmail("jyungoaa@sohei.com");
-    EXPECT_STREQ("jyungoaa@sohei.com", user.getEmail().c_str());
-    user.setPhone("999");
-    EXPECT_STREQ("999", user.getPhone().c_str());
+  User user;
+  user.setName("Clown");
+  EXPECT_STREQ("Clown", user.getName().c_str());
+  user.setPassword("55555555");
+  EXPECT_STREQ("55555555", user.getPassword().c_str());
+  user.setEmail("jyungoaa@sohei.com");
+  EXPECT_STREQ("jyungoaa@sohei.com", user.getEmail().c_str());
+  user.setPhone("999");
+  EXPECT_STREQ("999", user.getPhone().c_str());
 }
